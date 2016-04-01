76a2839dff403f7858cbdfc688070c68709ac437
Taha Dhiaeddine Amdouni <itdamdouni@gmail.com>
2016-04-01 23:20:56 +0200

    @network
    added folder @network

---
+++ @network/CFNetworkProxySettings.py
@@ -0,0 +1,13 @@
+# https://gist.github.com/omz/9b918c98d3f6bd66c1cc
+# Print system proxy settings using CFNetwork
+# NOTE: Requires Pythonista 1.6 (currently in beta)
+
+from objc_util import *
+
+CFNetworkCopySystemProxySettings = c.CFNetworkCopySystemProxySettings
+CFNetworkCopySystemProxySettings.restype = c_void_p
+CFNetworkCopySystemProxySettings.argtypes = []
+
+proxy_settings = ObjCInstance(CFNetworkCopySystemProxySettings())
+
+print proxy_settings
