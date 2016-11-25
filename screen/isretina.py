a10d762020fccf8e80a8fe2ec682cd74e9af51d6
Taha Dhiaeddine Amdouni <tdamdouni@users.noreply.github.com>
2016-11-25 09:26:52 +0100

    ...

---
+++ screen/isretina.py
@@ -0,0 +1,11 @@
+# https://gist.github.com/xen/01c913b4b13759e035880f74dcaa3f8f
+
+from objc_util import *
+us = ObjCClass('UIScreen')
+if us.mainScreen().scale() == 2.0:
+	print('Retina')
+elif us.mainScreen().scale() == 3.0:
+	print('iPhone 6 Plus')
+else:
+	print('Non retina')
+
