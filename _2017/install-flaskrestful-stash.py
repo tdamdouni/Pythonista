7d27337e96d871f4f90ab426a3bcfca0d627379a
Taha Dhiaeddine Amdouni <tdamdouni@users.noreply.github.com>
2017-07-28 10:30:13 +0200

    from iPad

---
+++ _2017/install-flaskrestful-stash.py
@@ -0,0 +1,5 @@
+# https://forum.omz-software.com/topic/4229/flask_restful-unable-to-install-with-stash
+
+$ wget "https://files.pythonhosted.org/packages/20/f1/14a62bba209ae189e5c5fa33d5e0b7a4b5969488fa71fd3b8b323860bfc8/Flask-RESTful-0.3.6.tar.gz"
+$ tar -xzf Flask-RESTful-0.3.6.tar.gz
+$ mv Flask-RESTful-0.3.6/flask_restful ~/Documents/site-packages/flask_restful
