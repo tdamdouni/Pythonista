32509aea5513415b65f94f32694becdeea0007a8
Taha Dhiaeddine Amdouni <tdamdouni@Peters-MBP.fritz.box>
2017-08-13 23:06:35 +0200

    upload from mac

---
+++ webview/worldmap/world.py
@@ -0,0 +1,14 @@
+import ui,os, urlparse
+class WorldDelegate (object):
+    def webview_should_start_load(self,webview, url, nav_type):
+        if url.startswith('world://'):
+            print 'country selected:', urlparse.unquote(urlparse.urlparse(url).netloc)
+            return False 
+        else:
+            return True  
+
+w=ui.WebView()
+w.delegate=WorldDelegate()
+p=os.path.abspath('world/index.html')
+w.load_url(p)
+w.present('panel')
